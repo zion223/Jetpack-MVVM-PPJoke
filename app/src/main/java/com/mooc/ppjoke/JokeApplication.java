f43e3bb7711e3d8df664149f3917e2e3a485cd3b
zhang <1025618933@qq.com>
2020-07-29 13:50:47 +0800

    初始化全局Context

--- app/src/main/java/com/mooc/ppjoke/JokeApplication.java
+++ app/src/main/java/com/mooc/ppjoke/JokeApplication.java
@@ -1,7 +1,8 @@
 package com.mooc.ppjoke;
 
-import android.app.Application;
 
+import com.kunminx.architecture.BaseApplication;
+import com.mooc.libarchitecture.utils.Utils;
 import com.mooc.libnetwork.ApiService;
 import com.tencent.bugly.crashreport.CrashReport;
 
@@ -13,10 +14,12 @@ import com.tencent.bugly.crashreport.CrashReport;
  * 同学们有需要的话,可以按照:https://git.imooc.com/Chubby/jetpack_ppjoke/src/master/%e6%9c%8d%e5%8a%a1%e5%99%a8%e7%8e%af%e5%a2%83%e6%90%ad%e5%bb%ba.md
  * 来搭建本地服务器
  */
-public class JokeApplication extends Application {
+public class JokeApplication extends BaseApplication {
+
     @Override
     public void onCreate() {
         super.onCreate();
+        Utils.init(this);
         ApiService.init("http://123.56.232.18:8080/serverdemo", null);
 
         CrashReport.initCrashReport(getApplicationContext(), "eb455a94a3", true);
